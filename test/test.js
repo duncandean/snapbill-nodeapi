18677dccc6a672f44b2e35a7f8d66c02ba4ba78b
Duncan Dean <duncangleeddean@gmail.com>
2018-11-10 15:38:52 +0200

    fix mocha things

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai');
+
+const foo = 'bar';
+const beverages = { tea: ['chai', 'matcha', 'oolong'] };
+
+assert.typeOf(foo, 'string'); // without optional message
+assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
+assert.equal(foo, 'bar', 'foo equal `bar`');
+assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
+assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
